58d56367f14f90abc82161bbca9360e545f64e9d
Scream <skate_max@mail.ru>
2020-07-29 12:23:38 +0300

    Updates

---
+++ precache-manifest.bd40936a9298151e182c3408eaa5d5b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9331bbe4e869d299b90d70d065fed2",
+    "url": "/exchange-rate/index.html"
+  },
+  {
+    "revision": "1ecdde58b5fb380c93ef",
+    "url": "/exchange-rate/static/css/main.1c5a22dd.chunk.css"
+  },
+  {
+    "revision": "f7c7ad8d424e73d5137f",
+    "url": "/exchange-rate/static/js/2.9e1bc149.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exchange-rate/static/js/2.9e1bc149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ecdde58b5fb380c93ef",
+    "url": "/exchange-rate/static/js/main.312c8a83.chunk.js"
+  },
+  {
+    "revision": "f366b2d41116f0af26c0",
+    "url": "/exchange-rate/static/js/runtime-main.ae23042d.js"
+  }
+]);
